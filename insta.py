afe4e824113bacb25b9a221b20d6d309ac11f9d8
Eric Machado <ericmachadoeng@gmail.com>
2019-04-28 00:13:45 -0300

    Melhorias e automacao do Bot

--- insta.py
+++ insta.py
@@ -2,59 +2,72 @@
 from instapy import InstaPy
 from instapy import smart_run
 
+import schedule
+import time
+
 # login credentials
-insta_username = "user"
-insta_password = "pass"
+insta_username = "login"
+insta_password = "senha"
+
+init_job = False
 
 # get an InstaPy session!
 # set headless_browser=True to run InstaPy in the background
 session = InstaPy(username=insta_username,
                   password=insta_password,
-                  headless_browser=False)
+                  headless_browser=True)
 
 with smart_run(session):
     """ Activity flow """
     # general settings
-    session.set_relationship_bounds(enabled=True,
-                                    delimit_by_numbers=True,
-                                    max_followers=4590,
-                                    min_followers=45,
-                                    min_following=77)
+    def jobInstaPy() :
+        session.set_relationship_bounds(enabled=True,
+                                        delimit_by_numbers=True,
+                                        max_followers=40000,
+                                        min_followers=70,
+                                        min_following=50)
 
-    session.set_dont_include(["friend1", "friend2", "friend3"])
-    session.set_dont_like(["pizza", "#store"])
 
-    # activities
 
-    """ Massive Follow of users followers (I suggest to follow not less than
-    3500/4000 users for better results)...
-    """
+        """ Massive Follow of users followers (I suggest to follow not less than
+        3500/4000 users for better results)...
+        """
 
-    to_follow = [#Array com perfis com publico alvo
-                ]
+        to_follow = ["Publico alvo",
+                    ]
+        session.set_skip_users(skip_private=True, private_percentage=100,
+        			 skip_no_profile_pic=True, no_profile_pic_percentage=100)
 
-    session.follow_user_followers(to_follow, amount=800,
-                                 randomize=False, interact=False)
+        session.follow_user_followers(to_follow, amount=800,
+                                     randomize=False, interact=False)
 
-    """ First step of Unfollow action - Unfollow not follower users...
-    """
-    session.unfollow_users(amount=500, InstapyFollowed=(True, "nonfollowers"),
-                           style="FIFO",
-                           unfollow_after=12 * 60 * 60, sleep_delay=601)
+        """ First step of Unfollow action - Unfollow not follower users...
+        """
+        session.unfollow_users(amount=800, InstapyFollowed=(True, "nonfollowers"),
+                               style="FIFO",
+                               unfollow_after=16 * 60 * 60, sleep_delay=601)
+        
 
-    """ Second step of Massive Follow...
-    """
-    session.follow_user_followers(to_follow, amount=800,
-                                randomize=False, interact=False)
+        """ Second step of Massive Follow...
+        """
+        session.follow_user_followers(to_follow, amount=800,
+                                    randomize=False, interact=False)
 
-    """ Second step of Unfollow action - Unfollow not follower users...
-    """
-    session.unfollow_users(amount=500, InstapyFollowed=(True, "nonfollowers"),
-                           style="FIFO",
-                           unfollow_after=12 * 60 * 60, sleep_delay=601)
+        """ Clean all followed user - Unfollow all users followed by InstaPy...
+        """
+        session.unfollow_users(amount=800, InstapyFollowed=(True, "all"),
+                            style="FIFO", unfollow_after = 32 * 60 * 60,
+                            sleep_delay=601)
+        time.sleep(10800)
 
-    """ Clean all followed user - Unfollow all users followed by InstaPy...
-    """
-    session.unfollow_users(amount=500, InstapyFollowed=(True, "all"),
-                           style="FIFO", unfollow_after=24 * 60 * 60,
-                           sleep_delay=601)
+
+
+if (not init_job):
+    jobInstaPy()
+    init_job = True
+
+schedule.every().day("06:00").do(jobInstaPy)
+
+while True:
+    schedule.run_pending()
+    time.sleep(300)
